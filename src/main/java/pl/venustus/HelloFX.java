d6f7c526f80bece25b4f847ccf33cbb1963477b8
venustuspl <venustus.pl@gmail.com>
2020-11-11 07:55:54 +0100

    Added new buttons and functions

--- src/main/java/pl/venustus/HelloFX.java
+++ src/main/java/pl/venustus/HelloFX.java
@@ -1,29 +1,65 @@
 package pl.venustus;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.time.LocalDateTime;
 
 public class HelloFX extends Application {
 
-    @Override
-    public void start(Stage stage) {
 
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ". LocalDateTime" + LocalDateTime.now());
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    // launch the application
+    public void start(Stage s)
+    {
+        // set title for the stage
+        s.setTitle("creating buttons");
+
+        // create a button
+        Button b = new Button("button");
+
+        // create a stack pane
+        TilePane r = new TilePane();
+
+        // create a label
+        Label l = new Label("button not selected");
+        Label l1 = new Label(String.valueOf(LocalDateTime.now()));
+
+        // action event
+        EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                l.setText("   button   selected    ");
+            }
+
+        };
+
+        // when button is pressed
+        b.setOnAction(event);
+
+        // add button
+        r.getChildren().add(b);
+        r.getChildren().add(l);
+        r.getChildren().add(l1);
+
+        // create a scene
+        Scene sc = new Scene(r, 200, 200);
+
+        // set the scene
+        s.setScene(sc);
+
+        s.show();
     }
 
-    public static void main(String[] args) {
-        launch();
-    }
 
+    public static void main(String args[])
+    {
+        // launch the application
+        launch(args);
+    }
 }
